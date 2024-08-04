686d03d148defeaad9b449fffa71c5026310460e
hongliang <hongliang.sun@hotmail.com>
2024-08-04 17:56:11 +1000

    Added body gesture inferencing and extraction

--- main.py
+++ main.py
@@ -67,22 +67,41 @@ def parseVideos():
 
 
 def captureLandmarks(image_folder):
-    base_options = python.BaseOptions(model_asset_path="./models/hand_landmarker.task")
-    options = vision.HandLandmarkerOptions(base_options=base_options, num_hands=2)
-    detector = vision.HandLandmarker.create_from_options(options)
+    # Hand Landmarks
+    hand_base_options = python.BaseOptions(
+        model_asset_path="./models/hand_landmarker.task"
+    )
+    hand_options = vision.HandLandmarkerOptions(
+        base_options=hand_base_options, num_hands=2
+    )
+    hand_detector = vision.HandLandmarker.create_from_options(hand_options)
+
+    # Body Landmarks
+    body_base_options = python.BaseOptions(
+        model_asset_path="./models/pose_landmarker_heavy.task"
+    )
+    body_options = vision.PoseLandmarkerOptions(
+        base_options=body_base_options, output_segmentation_masks=True
+    )
+    body_detector = vision.PoseLandmarker.create_from_options(body_options)
 
     # Load the image
     for filename in os.listdir(image_folder):
-        if filename.endswith(".jpg") or filename.endswith(".png"):
+        if (
+            filename.endswith(".jpg")
+            or filename.endswith(".png")
+            or filename.endswith(".jpeg")
+        ):
             image_path = os.path.join(image_folder, filename)
             image = mp.Image.create_from_file(image_path)
 
             # Detect hand landmarks from the input image.
-            detection_result = detector.detect(image)
+            hand_detection_result = hand_detector.detect(image)
+            body_detection_result = body_detector.detect(image)
 
             # Process the classification result. In this case, visualize it.
             annotated_image = draw_landmarks_on_image(
-                image.numpy_view(), detection_result
+                image.numpy_view(), hand_detection_result, body_detection_result
             )
             file_root, file_ext = os.path.splitext(filename)
             annotated_filename = f"{file_root}_annotated{file_ext}"
@@ -92,24 +111,27 @@ def captureLandmarks(image_folder):
                 cv2.cvtColor(annotated_image, cv2.COLOR_RGB2BGR),
             )
 
-            save_landmarks_to_file(image_folder, filename, detection_result)
+            save_landmarks_to_file(
+                image_folder, filename, hand_detection_result, body_detection_result
+            )
 
 
-def draw_landmarks_on_image(rgb_image, detection_result):
-    MARGIN = 10  # pixels
+def draw_landmarks_on_image(rgb_image, hand_detection_result, body_detection_result):
+    MARGIN = 10
     FONT_SIZE = 1
     FONT_THICKNESS = 1
     HANDEDNESS_TEXT_COLOR = (88, 205, 54)
-    hand_landmarks_list = detection_result.hand_landmarks
-    handedness_list = detection_result.handedness
+
+    hand_landmarks_list = hand_detection_result.hand_landmarks
+    handedness_list = hand_detection_result.handedness
+    pose_landmarks_list = body_detection_result.pose_landmarks
+
     annotated_image = np.copy(rgb_image)
 
     # Loop through the detected hands to visualize.
     for idx in range(len(hand_landmarks_list)):
         hand_landmarks = hand_landmarks_list[idx]
         handedness = handedness_list[idx]
-
-        # Draw the hand landmarks.
         hand_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
         hand_landmarks_proto.landmark.extend(
             [
@@ -146,26 +168,58 @@ def draw_landmarks_on_image(rgb_image, detection_result):
             cv2.LINE_AA,
         )
 
+    for idx in range(len(pose_landmarks_list)):
+        pose_landmarks = pose_landmarks_list[idx]
+        pose_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
+        pose_landmarks_proto.landmark.extend(
+            [
+                landmark_pb2.NormalizedLandmark(
+                    x=landmark.x, y=landmark.y, z=landmark.z
+                )
+                for landmark in pose_landmarks
+            ]
+        )
+        solutions.drawing_utils.draw_landmarks(
+            annotated_image,
+            pose_landmarks_proto,
+            solutions.pose.POSE_CONNECTIONS,
+            solutions.drawing_styles.get_default_pose_landmarks_style(),
+        )
+
     return annotated_image
 
 
-def save_landmarks_to_file(image_folder, filename, detection_result):
-    hand_landmarks_list = detection_result.hand_landmarks
-    handedness_list = detection_result.handedness
+def save_landmarks_to_file(
+    image_folder, filename, hand_detection_result, body_detection_result
+):
+    hand_landmarks_list = hand_detection_result.hand_landmarks
+    handedness_list = hand_detection_result.handedness
+    body_landmarks_list = body_detection_result.pose_landmarks
+
     file_root, _ = os.path.splitext(filename)
     landmark_filename = f"{file_root}_landmarks.txt"
     landmark_filepath = os.path.join(image_folder, landmark_filename)
 
     with open(landmark_filepath, "w") as f:
-        for idx in range(len(hand_landmarks_list)):
-            hand_landmarks = hand_landmarks_list[idx]
-            handedness = handedness_list[idx][0].category_name
-            f.write(f"Hand: {handedness}\n")
-            for landmark in hand_landmarks:
-                f.write(f"{landmark.x}, {landmark.y}, {landmark.z}\n")
-        # Fill with 0 if no hand landmarks detected
-        if not hand_landmarks_list:
+        if hand_landmarks_list:
+            for idx in range(len(hand_landmarks_list)):
+                hand_landmarks = hand_landmarks_list[idx]
+                handedness = handedness_list[idx][0].category_name
+                f.write(f"Hand: {handedness}\n")
+                for landmark in hand_landmarks:
+                    f.write(f"{landmark.x}, {landmark.y}, {landmark.z}\n")
+        else:
             for _ in range(21):  # 21 landmarks per hand
+                f.write("0, 0, 0\n")
+
+        # Write body landmarks
+        if body_landmarks_list:
+            for idx in range(len(body_landmarks_list)):
+                f.write("Body:\n")
+                for landmark in body_landmarks_list[idx]:
+                    f.write(f"{landmark.x}, {landmark.y}, {landmark.z}\n")
+        else:
+            for _ in range(33):  # 33 landmarks for body
                 f.write("0, 0, 0\n")
 
 
@@ -180,7 +234,7 @@ if __name__ == "__main__":
     mp_holistic = mp.solutions.holistic
 
     # Convert all videos in /raw_data/videos to frames
-    # parseVideos()
+    parseVideos()
 
     # test
-    captureLandmarks("raw_data/frames/testing")
+    # captureLandmarks("raw_data/frames/testing")
---
+++ raw_data/plotVisualisation.py
@@ -0,0 +1,52 @@
+########################################################################################
+# This script reads the landmarks from a text file and plots them on a graph.
+# The text file should contain the landmarks in the following format:
+# x1, y1, z1
+# x2, y2, z2
+# ...
+# xn, yn, zn
+# where x, y, and z are the coordinates of the landmarks.
+# any line that cannot be converted to float will be skipped, i.e. Right Left Body.
+########################################################################################
+
+
+import matplotlib.pyplot as plt
+import os
+
+
+def read_points(file_path):
+    points = []
+    with open(file_path, "r") as file:
+        for line in file:
+            line = line.strip()
+            try:
+                x, y, z = map(float, line.split(","))
+                points.append((x, -y))  # Invert the y-axis
+            except ValueError:
+                # Skip lines that cannot be converted to float
+                continue
+    return points
+
+
+def plot_points(points):
+    fig, ax = plt.subplots()
+
+    xs = [point[0] for point in points]
+    ys = [point[1] for point in points]
+
+    ax.scatter(xs, ys, c="b", marker="o")
+
+    ax.set_xlabel("X Label")
+    ax.set_ylabel("Y Label")
+
+    plt.show()
+
+
+if __name__ == "__main__":
+    # Change the path to the file you want to plot manually
+    file_name = "./frames/testing/Untitled1_landmarks.txt"
+
+    curr_dir = os.path.dirname(os.path.realpath(__file__))
+    file_path = os.path.join(curr_dir, file_name)
+    points = read_points(file_path)
+    plot_points(points)
